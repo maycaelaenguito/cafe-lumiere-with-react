0f510b9304b29d2cad2b4eead928351471eea484
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-07-15 23:45:34 -0400

    added few changes

--- src/components/BgSlideshow.js
+++ src/components/BgSlideshow.js
@@ -4,19 +4,11 @@ import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 
 const BgSlideshow = () => {
-  const handleHover = (event) => {
-    event.target.style.color = '#42180ab2';
-  };
-
-  const handleMouseLeave = (event) => {
-    event.target.style.color = ''; // Reset the color to default when not hovered
-  };
 
   return (
     <div className='slide-container'>
       {/* AUTOPLAY SLIDESHOW BG-IMAGE */}
-      <div className="header-bg">
-      </div> 
+      <div className="header-bg"></div> 
       {/* HEADER CONTENT/separate the bg-image and the content to make the filter take effect specifically on the bg-image only */}
       <div className="nav">
         <Navbar/>
@@ -31,20 +23,14 @@ const BgSlideshow = () => {
         <div className='header-btn'>
           <div className='menu'>
             <Link to="/about/menu">
-              <button
-                onMouseEnter={handleHover}
-                onMouseLeave={handleMouseLeave}
-              >
+              <button>
                 See Full Menu
               </button>
             </Link>
           </div>
           <div className='booking'>
             <Link to="/contactus/booking">
-              <button
-                onMouseEnter={handleHover}
-                onMouseLeave={handleMouseLeave}
-              >
+              <button>
                 Book A Table
               </button>
             </Link>
@@ -52,7 +38,7 @@ const BgSlideshow = () => {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default BgSlideshow;
