6d81a97987cd5cca6851af40ff9f5633c289d933
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-07-27 05:49:35 -0400

    commit

---
+++ src/components/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div className='gallery-container'>
+      <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Gallery
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import Header from './Header';
 import MenuComponent from './MenuComponent';
 import BgSlideshow from './BgSlideshow';
 import ReviewsSlider from './ReviewsSlider';
-import ImageScroll from './ImageScroll';
 
-import image1 from '../assets/1(1).png';
-import image2 from '../assets/2(1).png';
-import image3 from '../assets/3(1).png';
-import image4 from '../assets/4.png';
-import image5 from '../assets/5.png';
-import image6 from '../assets/6.png';
-import image7 from '../assets/6.png';
 
-const images = [image1, image2, image3, image4, image5, image6, image7,];
+// import image1 from '../assets/1(1).png';
+// import image2 from '../assets/2(1).png';
+// import image3 from '../assets/3(1).png';
+// import image4 from '../assets/4.png';
+// import image5 from '../assets/5.png';
+// import image6 from '../assets/6.png';
+// import image7 from '../assets/6.png';
+
+// const images = [image1, image2, image3, image4, image5, image6, image7,];
 
 const Home = () => {
   return (
@@ -38,7 +38,6 @@ const Home = () => {
           </p>
         </div>
       </div>
-      <ImageScroll images={images} />
     </div>
   );
 };
