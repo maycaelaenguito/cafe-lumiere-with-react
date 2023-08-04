8e0c16c58060b7a77a0c6c8b02932f7130d0d0b7
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-04 06:14:23 -0400

    added blogs

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,11 +1,72 @@
 import React from 'react';
 import Navbar from './Navbar';
+import '../css/blogs.css';
 
 const Blogs = () => {
   return (
-    <div className='blogs'>
-      <Navbar/>
-      <h1>Blogs</h1>
+    <div>
+      <div className="blogs-nav">
+        <Navbar/>
+      </div>
+      <div className='blogs-header'>
+          <div className="blogs-container">
+            {/* First Blog */}
+            <div className="chronicles blogs">
+              <div className="chronicles-img">
+              </div>
+              <div className="blog-title">
+                <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+              </div>
+              <div className="blog-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+            {/* Second Blog */}
+            <div className="bliss blogs">
+              <div className="bliss-img">
+              </div>
+              <div className="blog-title">
+                <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+              </div>
+              <div className="blog-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+            </div>
+            {/* Third Blog */}
+            <div className="delights blogs">
+              <div className="chronicles-img">
+                </div>
+                <div className="blog-title">
+                  <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+                </div>
+                <div className="blog-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            {/* Fourth Blog */}
+            <div className="culture blogs">
+              <div className="chronicles-img">
+                </div>
+                <div className="blog-title">
+                  <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+                </div>
+                <div className="blog-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            {/* Fifth Blog */}
+            <div className="temptations blogs">
+              <div className="chronicles-img">
+                </div>
+                <div className="blog-title">
+                  <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+                </div>
+                <div className="blog-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,8 +84,10 @@ const Navbar = () => {
                 <ul>
                     <li> <Link to="/blogs/blog-one">Coffee Chronicles</Link> </li>
                     <li> <Link to="/blogs/blog-two">Brewing Bliss</Link> </li>
-                    <li> <Link to="/blogs/blog-three">Bean Buzz</Link> </li>
+                    <li> <Link to="/blogs/blog-three">Divine Delights</Link> </li>
                     <li> <Link to="/blogs/blog-four">Caffeine Culture</Link> </li>
+                    <li> <Link to="/blogs/blog-four">Sweet Temptations</Link> </li>
+
                   </ul>
                </div>
               </li>
