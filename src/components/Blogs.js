14baee44128b95dbf9c1c13e6bf0dc9d527ad16a
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-06 20:27:28 -0400

    added blogs

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -16,7 +16,7 @@ const Blogs = () => {
               <div className="chronicles-img">
               </div>
               <div className="blog-title">
-                <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+                <h1>Coffee Chronicles: Sipping through Time and Cultures</h1>
               </div>
               <div className="blog-description">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -27,10 +27,10 @@ const Blogs = () => {
               <div className="bliss-img">
               </div>
               <div className="blog-title">
-                <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+                <h1>Unveiling the Art of Brewing Blissful Cups at Home</h1>
               </div>
               <div className="blog-description">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur tellus euismod, luctus enim id, venenatis velit. Sed cursus, neque quis vulputate tristique, augue elit consectetur elit. Suspendisse ornare ligula eu semper tempor. Fusce interdum, sapien ac fermentum vehicula, mi enim tincidunt neque, a blandit mi orci a urna. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque congue volutpat elit ac gravida. Aliquam eget dui at felis pellentesque tincidunt.</p>
               </div>
             </div>
             {/* Third Blog */}
@@ -38,10 +38,10 @@ const Blogs = () => {
               <div className="chronicles-img">
                 </div>
                 <div className="blog-title">
-                  <h1>The Coffee Chronicles: Sipping through Time and Cultures</h1>
+                  <h1>10 Most Popular Pastries in Europe</h1>
                 </div>
                 <div className="blog-description">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique nulla ac nisi venenatis, id rhoncus elit tincidunt. Cras mattis nibh vitae dolor.</p>
                 </div>
             </div>
             {/* Fourth Blog */}
