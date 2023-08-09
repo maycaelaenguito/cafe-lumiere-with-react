ed0159124ef951d43aa4fca1c47da93d96d0d880
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-09 02:05:51 -0400

    commit

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Navbar from './Navbar';
+import '../css/contactus.css'
 
 
 const ContactUs = () => {
   return (
-    <div className='contactus'>
+    <div className='contactus-nav'>
         <Navbar/>
-      <h1>Contact Us</h1>
+    <div className="contactus-container">
+      <form action="https://getform.io/f/fb1a06c3-1dcb-48cd-a078-8bf75ab83af8" method="POST">
+          <div className='form-item'>
+            <label name="name">Full Name <span>*</span></label>
+            <input type="text" name="name" placeholder='e.g., John Doe' required />
+          </div>
+          <div className="form-item">
+            <label name="email">Email Address <span>*</span></label>
+            <input type="email" name="email" placeholder='user@example.com' required />
+          </div>
+          <div className="form-item">
+            <label name="message">Message <span>*</span></label>
+            <div className='center'>
+              <textarea name="message" id="" required></textarea>
+            </div>
+          </div>
+          <div className="form-btn">
+            <button type="submit">Send</button>
+          </div>
+      </form>
+    </div>
     </div>
   )
 }
