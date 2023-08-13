d9d7e15b9f16a1af0555efb7f15975af1eae5785
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-12 22:16:15 -0400

    commit

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Booking = () => {
+  const [numPersons, setNumPersons] = useState(1);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedTime, setSelectedTime] = useState('');
+
+  const handleNumPersonsChange = (event) => {
+    setNumPersons(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const handleTimeChange = (event) => {
+    setSelectedTime(event.target.value);
+  };
+
   return (
     <div>
-      <h1>Book Now</h1>
+      <h2>Reserve a Seat</h2>
+      <div>
+        <label htmlFor="numPersons">Number of Persons:</label>
+        <select id="numPersons" value={numPersons} onChange={handleNumPersonsChange}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          {/* Add more options if needed */}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="selectedDate">Date:</label>
+        <input
+          type="date"
+          id="selectedDate"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="selectedTime">Time:</label>
+        <input
+          type="time"
+          id="selectedTime"
+          value={selectedTime}
+          onChange={handleTimeChange}
+        />
+      </div>
+      <div>
+        <p>Selected: {numPersons} persons on {selectedDate} at {selectedTime}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Booking
+export default Booking;
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
-import '../css/contactus.css'
+import '../css/contactus.css';
+import Booking from './Booking';
 
 
 const ContactUs = () => {
@@ -10,28 +11,27 @@ const ContactUs = () => {
         <Navbar/>
       </div>
       <div className='contactus-header'>
-        <p>Contact Us</p>
-      </div>
-      <div className="contactus-container">
-        <form action="https://getform.io/f/fb1a06c3-1dcb-48cd-a078-8bf75ab83af8" method="POST">
-            <div className='form-item'>
-              <label name="name">Full Name <span>*</span></label>
-              <input type="text" name="name" placeholder='e.g., John Doe' required />
-            </div>
-            <div className="form-item">
-              <label name="email">Email Address <span>*</span></label>
-              <input type="email" name="email" placeholder='user@example.com' required />
-            </div>
-            <div className="form-item">
-              <label name="message">Message <span>*</span></label>
-              <div className='center'>
-                <textarea name="message" id="" required></textarea>
+        <div className="contactus-content">
+
+          {/* Booking */}
+            <div className="contactus-booking">
+              <div className="booking-container">
+                <Booking/>
               </div>
             </div>
-            <div className="form-btn">
-              <button type="submit">Send</button>
-            </div>
-        </form>
+
+          {/* Form */}
+          <div className="contact-details">
+           <h1>Reach Us</h1>
+           <div className=''>
+
+           </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="contactus-container">
+       
       </div>
     </div>
   )
