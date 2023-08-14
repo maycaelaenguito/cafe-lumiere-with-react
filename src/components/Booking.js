1af674d5fbd3b8b78e6bd6a6940e881b3fe8a12a
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-13 23:11:13 -0400

    added booking dropdowns

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../css/booking.css';
 import CustomDropdown from './CustomDropdown';
 import CustomCalendar from './CustomCalendar';
-
-
+import TimePicker from './TimePicker';
 
 const Booking = () => {
 
-  const [selectedTime, setSelectedTime] = useState('');
-
-  const handleTimeChange = (event) => {
-    setSelectedTime(event.target.value);
-  };
 
   return (
     <div>
@@ -22,14 +16,8 @@ const Booking = () => {
       <div className='booking-date'>
        <CustomCalendar/>
       </div>
-      <div>
-        <label htmlFor="selectedTime">Time:</label>
-        <input
-          type="time"
-          id="selectedTime"
-          value={selectedTime}
-          onChange={handleTimeChange}
-        />
+      <div className='time-picker'>
+       <TimePicker/>
       </div>
     </div>
   );
--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "../css/customdropdown.css"; // Import your custom CSS for styling
 
-const CustomDropdown = () => {
+const TimePicker = () => {
   const [selectedValue, setSelectedValue] = useState(1); // Initial selected value
   const [isOpen, setIsOpen] = useState(false);
   const [dropUp, setDropUp] = useState(false); // Added state to track if dropdown should appear above
@@ -32,16 +32,28 @@ const CustomDropdown = () => {
   };
 
   const options = [
-    { value: 1, label: "1 Person" },
-    { value: 2, label: "2 People" },
-    { value: 3, label: "3 People" },
-    { value: 4, label: "4 People" },
-    { value: 5, label: "5 People" },
-    { value: 6, label: "6 People" },
-    { value: 7, label: "7 People" },
-    { value: 8, label: "8 People" },
-    { value: 9, label: "9 People" },
-    { value: 10, label: "10 People" },
+    { value: 1, label: "10:00 am" },
+    { value: 2, label: "10:30 am" },
+    { value: 3, label: "11:00 am" },
+    { value: 4, label: "11:30 am" },
+    { value: 5, label: "12:00 pm" },
+    { value: 6, label: "12:30 pm" },
+    { value: 7, label: "1:00 pm" },
+    { value: 8, label: "1:30 pm" },
+    { value: 9, label: "2:00 pm" },
+    { value: 10, label: "2:30 pm" },
+    { value: 11, label: "3:00 pm" },
+    { value: 12, label: "3:30 pm" },
+    { value: 13, label: "4:00 pm" },
+    { value: 14, label: "4:30 pm" },
+    { value: 15, label: "5:00 pm" },
+    { value: 16, label: "5:30 pm" },
+    { value: 17, label: "6:00 pm" },
+    { value: 17, label: "6:30 pm" },
+    { value: 17, label: "7:00 pm" },
+    { value: 17, label: "7:30 pm" },
+    { value: 17, label: "8:00 pm" },
+    { value: 17, label: "8:30 pm" },
   ];
 
   return (
@@ -72,4 +84,4 @@ const CustomDropdown = () => {
   );
 };
 
-export default CustomDropdown;
+export default TimePicker;
