f447dc09f8079a780dad29525d2318247ea3338a
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-17 04:52:51 -0400

    commit

--- src/components/ContactDets.js
+++ src/components/ContactDets.js
@@ -6,43 +6,47 @@ import GoogleMaps from './GoogleMaps';
 const ContactDets = () => {
   return (
     <div className="contactus-container">
-        <div className="contact-wrapper">
-            <div className="contact-details">
-                <h3>Reach Us</h3>
-                <div className='details'>
-                    <div className="e-address contact-info">
-                        <div>
-                            <h4>Email</h4>
-                        </div>
-                        <div>
-                            <p>info@cafelumiere.com</p>
-                        </div>
-                    </div>
-                    <div className="phone-number contact-info">
-                        <div>
-                            <h4>Phone</h4>
-                        </div>
-                        <div>
-                            <p>(05) 3888-9920</p>
-                        </div>
-                    </div>
-                    <div className="address contact-info">
-                        <div>
-                            <h4>Address</h4>
-                        </div>
-                        <div>
-                            <p>987 Araneta Avenue, Quezon City, Metro Manila, Philippines</p>
+      <div className="full">
+        <div className="cnt">
+            <div className="contact-wrapper">
+                    <div className="contact-details">
+                        <h3>Reach Us</h3>
+                        <div className='details'>
+                            <div className="e-address contact-info">
+                                <div>
+                                    <h4>Email</h4>
+                                </div>
+                                <div>
+                                    <p>info@cafelumiere.com</p>
+                                </div>
+                            </div>
+                            <div className="phone-number contact-info">
+                                <div>
+                                    <h4>Phone</h4>
+                                </div>
+                                <div>
+                                    <p>(05) 3888-9920</p>
+                                </div>
+                            </div>
+                            <div className="address contact-info">
+                                <div>
+                                    <h4>Address</h4>
+                                </div>
+                                <div>
+                                    <p>987 Araneta Avenue, Quezon City, Metro Manila, Philippines</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                {/* Map */}
+                <div className="map">
+                    <div className='gmap-wrapper'>
+                        <GoogleMaps />
+                    </div>
+                </div>
         </div>
-        
-
-        {/* Map */}
-        <div className="map">
-            <GoogleMaps />
-        </div>
+      </div>
 
 
         {/* NewsLetter */}
