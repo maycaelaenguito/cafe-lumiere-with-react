18f0e301c283c2b8daa381bede1f317b8f2565f7
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-18 04:24:02 -0400

    commit

--- src/components/BlogsBtn.js
+++ src/components/BlogsBtn.js
@@ -4,7 +4,7 @@ import '../css/buttons.css'
 
 const BlogsBtn = () => {
 return (
-    <div className='menu menu-btn' style={{marginBottom: '4rem'}}>
+    <div className='menu menu-btn blg-btn' style={{marginBottom: '4rem'}}>
         <Link to="/blogs">
             <button>
             Read More
