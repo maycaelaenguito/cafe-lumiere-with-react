600869efe7d77c0de31e50bc7f8cba3344700dbc
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-25 06:45:19 -0400

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,14 @@ const About = () => {
       <div className="about-header">
         <div className='a-hder-con'>
           <div className="a-left-header a-hder-items">
-            <p>Left</p>
+              <div className='a-header-title'>
+                <p>Exquisite Cuisine</p>
+              </div>
+              <div className="a-header-des">
+                <h3>Journey to a bygone era of elegance at Cafe Lumiere, a retro-themed haven where timeless charm meets refined indulgence.</h3>
+                <p className='a-header-sub'>Nestled within the heart of the city, Cafe Lumiere beckons visitors with its tranquil ambiance. Sun-kissed walls adorned with captivating artwork, the scent of freshly brewed coffee filling the air, and cozy corners that invite quiet contemplation, this haven promises moments of respite and inspiration.</p>
+                <p>Discover a picturesque retreat from the bustling city. Bathed in soft, natural light, the café's elegant interior boasts vibrant artwork, creating an immersive gallery-like experience.</p>
+              </div>
           </div>
           <div className="a-right-header a-hder-items">
           </div>
