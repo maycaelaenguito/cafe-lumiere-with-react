3213f1f40a5bcfa125df9885a6d7c797a0ed1d0d
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-26 07:02:08 -0400

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -18,9 +18,9 @@ const About = () => {
                 <p>Exquisite Cuisine</p>
               </div>
               <div className="a-header-des">
-                <h3>Journey to a bygone era of elegance at Cafe Lumiere, a retro-themed haven where timeless charm meets refined indulgence.</h3>
-                <p className='a-header-sub'>Nestled within the heart of the city, Cafe Lumiere beckons visitors with its tranquil ambiance. Sun-kissed walls adorned with captivating artwork, the scent of freshly brewed coffee filling the air, and cozy corners that invite quiet contemplation, this haven promises moments of respite and inspiration.</p>
-                <p>Discover a picturesque retreat from the bustling city. Bathed in soft, natural light, the café's elegant interior boasts vibrant artwork, creating an immersive gallery-like experience.</p>
+                <h3>Nestled at the heart of urban allure, Cafe Lumière beckons you into a world where gastronomy meets artistry, and where each cup of meticulously crafted coffee tells a story. </h3>
+                <p className='a-header-sub'>Nestled at the heart of urban allure, Cafe Lumière beckons you into a world where gastronomy meets artistry, and where each cup of meticulously crafted coffee tells a story. Step into an inviting haven where the soft glow of vintage chandeliers dances upon exposed brick walls, seamlessly blending the nostalgic charm of yesteryears with the vibrancy of contemporary culture. At Cafe Lumière, we are more than just a coffeehouse; we are curators of flavor, purveyors of passion, and connoisseurs of comfort. Drawing inspiration from the ethereal interplay of light and shadow, our menu is a symphony of flavors that transcends boundaries, celebrating the harmonious union of diverse culinary traditions.</p>
+                <p>More than a mere cafe, we are the embodiment of an ambiance where moments are woven into memories, where conversations linger, and where time itself becomes a treasured guest. Come, bask in the warm embrace of Cafe Lumière, where the essence of culinary delight and cultural fusion intertwine, casting a radiant glow that transcends the ordinary.</p>
               </div>
           </div>
           <div className="a-right-header a-hder-items">
