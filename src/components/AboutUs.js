74aa9f4e6be20939e06a7cba13f8e72a071d9c6b
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-08-30 06:31:37 -0400

    commit

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -15,8 +15,19 @@ const AboutUs = () => {
         <div className="team-wrapper">
           <div>
             <div>
-              <h1>Meet The Crew</h1>
-              <p>At the heart of our offerings is the seamless synergy of our team. With a shared love for crafting exquisite cuisine, brewing comforting coffee, and delivering impeccable service, we're here to elevate your every experience.</p>
+                <div className='team-ttl team-item'>
+                  <h1>Meet The Crew</h1>
+                </div>
+                <div className='team-desc team-item'>
+                  <p>At the heart of our offerings is the seamless synergy of our team. With a shared love for crafting exquisite cuisine, brewing comforting coffee, and delivering impeccable service, we're here to elevate your every experience.</p>
+                </div>
+            </div>
+            <div className="team-profile">
+            <div class="card">
+              <div class="content">
+                  <h3>Lily Green</h3>
+              </div>
+            </div>
             </div>
           </div>
         </div>
