6b78571f0f921ec123b221fbd1a0b2005cb95ad5
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-02 10:33:11 -0400

    commit

--- src/components/TodaysMenu.js
+++ src/components/TodaysMenu.js
@@ -16,42 +16,48 @@ const TodaysMenu = () => {
             <div className="tmenu-wrapper">
               <div className="tmenu-title tmenu-title1">
                 <h4>Main Dishes</h4>
-                <ul>
-                  <li>Mediterranean Chicken Salad</li>
-                  <li>Italian Truffle Mushroom Risotto</li>
-                  <li>French Croque Monsieur</li>
-                </ul>
+                <div className="menulist">
+                  <ul>
+                    <li>Mediterranean Chicken Salad</li>
+                    <li>Italian Truffle Mushroom Risotto</li>
+                    <li>French Croque Monsieur</li>
+                  </ul>
+                </div>
               </div>
               <div className="tmenu tmenu-img1">
-                <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693661720/Cafe%20Lumiere/1_2_r1wk7x.png" alt="Main Dish" />
+                <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693662694/Cafe%20Lumiere/1_3_ajets9.png" alt="Main Dish" />
               </div>
             </div>
             {/*  */}
             <div className="tmenu-wrapper">
               <div className="tmenu-title tmenu-title2">
                 <h4>Desserts</h4>
-                <ul>
-                  <li>Tiramisu Parfait Topped with Strawberries</li>
-                  <li>French Macaron Assortment</li>
-                  <li>German Black Forest Cake</li>
-                </ul>
+                <div className="menulist">
+                  <ul>
+                    <li>Tiramisu Parfait Topped with Strawberries</li>
+                    <li>French Macaron Assortment</li>
+                    <li>German Black Forest Cake</li>
+                  </ul>
+                </div>
               </div>
               <div className="tmenu tmenu-img2">
-                <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693661720/Cafe%20Lumiere/2_2_niwyoh.png" alt="Desserts" />
+                <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693662695/Cafe%20Lumiere/2_3_yjnnrq.png" alt="Desserts" />
               </div>
             </div>
             {/*  */}
             <div className="tmenu-wrapper">
               <div className="tmenu-title tmenu-title3">
                 <h4>Beverages</h4>
-                <ul>
-                  <li>Café au Lait</li>
-                  <li>Spanish Sangria</li>
-                  <li>Aperol Spritz</li>
-                </ul>
+                <div className="menulist">
+                  <ul>
+                    <li>Café au Lait <span className='label-hash hash7'></span> 150</li>
+                    <li>Spanish Sangria <span className='label-hash hash8'></span> 200</li>
+                    <li>Aperol Spritz <span className='label-hash hash9'></span> 250</li>
+                  </ul>
+                </div>
               </div>
               <div className="tmenu tmenu-img3">
-                <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693661720/Cafe%20Lumiere/3_2_fmfuvy.png" alt="Beverages" />
+                <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693662693/Cafe%20Lumiere/3_3_yujvte.png" alt="Beverages" />
               </div>
             </div>
           </div>
