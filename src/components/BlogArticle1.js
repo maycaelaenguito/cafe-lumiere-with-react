d5944bbd26796a1f34198b68ecf4b1bf72c525bc
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-06 06:52:26 -0400

    commit

--- src/components/BlogArticle1.js
+++ src/components/BlogArticle1.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import BacktoTop from './BacktoTop';
+import Footer from './Footer';
 
 const BlogArticle1 = () => {
   return (
     <div className='blog-1'>
-      <h1>Blog1</h1>
+      <div className="blogs-nav">
+        <Navbar/>
+      </div>
+      <div className="blog-posts">
+        <div className="main-article">
+          {/* <div className="blog-subheader-title">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div> */}
+          <div className="post-img">
+            <img src="https://res.cloudinary.com/dk3aalcb0/image/upload/v1693996461/Cafe%20Lumiere/matchmocha_qke1ut.png" alt="Blog 1" />
+          </div>
+          <div className="blog-date">
+                <p>07 / 22 / 2023</p>
+          </div>
+          <div className="blog-header-title">
+            <h1>Matcha vs. Mocha: Which Brew is Right for You?</h1>
+          </div>
+          <div className="blog-content">
+            <div className="blogs-sub">
+              <p>In the world of caffeinated beverages, there's no shortage of options to choose from. Two popular choices that often find themselves in the spotlight are matcha and mocha. While they may sound similar, they are distinctly different in terms of flavor, preparation, and health benefits. So, if you're wondering which brew is right for you, let's dive into the matcha vs. mocha showdown and help you make an informed decision.</p>
+             <div className='blogs-item'>
+              <h2>Matcha: The Green Elixir</h2>
+                <p><span>Origins and History: </span>Matcha is a traditional Japanese powdered green tea that has been around for centuries. It's prepared by grinding shade-grown green tea leaves into a fine powder. The Japanese tea ceremony, known as Chanoyu, has deep roots in matcha consumption, emphasizing mindfulness and appreciation of the tea.</p>
+                <p><span>Flavor Profile:  </span>Matcha has a distinctive, grassy, and slightly bitter flavor with sweet undertones. It's often described as earthy and vegetal, and the taste can vary depending on the quality and grade of matcha.</p>
+                <p><span>Preparation:   </span>Preparing matcha requires a special process. You whisk the powdered tea with hot water to create a frothy, vibrant green drink. Traditionally, it's served in a small bowl, and the whole experience is both meditative and ceremonial.</p>
+                <p><span>Health Benefits:  </span>Matcha is packed with antioxidants, particularly catechins, which are believed to have various health benefits, including improved focus and a boost to metabolism. It also contains chlorophyll, vitamins, and minerals, making it a nutritional powerhouse.</p>
+             </div>
+             <div className='blogs-item'>
+              <h2>Mocha: The Indulgent Delight</h2>
+                <p><span>Origins and History: </span>Mocha, unlike matcha, is not a traditional beverage with deep-rooted cultural ties. Instead, it's a modern creation that combines coffee and chocolate, originating from Mocha beans in Yemen and popularized in the Western world.</p>
+                <p><span>Flavor Profile:  </span>Mocha is a delightful blend of rich coffee and sweet chocolate. It's a comforting and indulgent drink, perfect for those with a sweet tooth. The flavor can vary depending on the ratio of coffee to chocolate and the type of chocolate used.</p>
+                <p><span>Preparation:   </span>Making a mocha is quite simple. It typically involves combining espresso or strong coffee with hot milk and sweetened cocoa powder or chocolate syrup. Whipped cream and chocolate shavings are often added as a delicious topping.</p>
+                <p><span>Health Benefits:  </span>While mochas are undeniably delicious, they are not known for their health benefits. The sugar and calorie content in a typical mocha can be high, especially if it's loaded with whipped cream and extra chocolate. However, the caffeine in coffee can provide a temporary energy boost.</p>
+             </div>
+             <div className="blog-conclusion">
+              <h2>Which One is Right for You?</h2>
+              <p>Choosing between matcha and mocha ultimately depends on your personal preferences and needs.</p>
+              <span>Choose Matcha if:</span>
+              <ol>
+                <li>You appreciate the meditative and ceremonial aspects of tea.</li>
+                <li>You enjoy the natural, earthy flavors of green tea.</li>
+                <li>You're looking for a healthier beverage with antioxidants and potential health benefits.</li>
+                <li>You prefer a caffeine boost without the jitters associated with coffee.</li>
+              </ol>
+              <span>Choose Mocha if:</span>
+              <ol>
+                <li>You have a sweet tooth and crave a more indulgent, dessert-like experience.</li>
+                <li>You're a coffee lover and want the rich, bold flavors of coffee with a hint of chocolate.</li>
+                <li>You need a pick-me-up during a busy day, as mochas can provide a quick energy boost.</li>
+                <li>You're not particularly fond of the grassy taste of green tea.</li>
+              </ol>
+              <p>In the matcha vs. mocha battle, there's no definitive winner, as it all comes down to personal taste and what you're seeking in your cup. Whether you opt for the serene elegance of matcha or the comforting allure of mocha, both of these beverages have their own unique charm. So, the next time you find yourself at a cafe, take a moment to reflect on your mood and cravings, and make your choice accordingly. Cheers to enjoying every sip of your favorite brew!</p>
+             </div>
+            </div>
+          </div>
+        </div>
+        <div className="side-article">
+
+        </div>
+      </div>
+      <div className="floating-btn">
+        <BacktoTop />
+      </div>
+      <Footer/>
     </div>
   )
 }
